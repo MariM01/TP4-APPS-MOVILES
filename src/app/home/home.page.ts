9b2c6cc1bfc8beba3ab6de6677cf2b7990432c1e
Nicolas Pieroni <nicolas.pieroni@corsisa.com.ar>
2023-05-21 18:59:40 -0300

    Proyecto terminado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Storage } from '@capacitor/storage';
 
 
 @Component({
@@ -14,17 +15,36 @@ export class HomePage {
   public personajes: any[] = [];
   public personaje: any;
   public isModalOpen: boolean = false;
+  private readonly STORAGE_KEY = 'personajes';
 
-  constructor(private http: HttpClient, private router: Router, private alertController: AlertController) { }
 
+  constructor(private http: HttpClient, private router: Router, private alertController: AlertController) { 
+    this.cargarPersonajes();
+  }
+
+  public async guardarPersonajes() {
+    await Storage.set({
+      key: this.STORAGE_KEY,
+      value: JSON.stringify(this.personajes)
+    });
+  }
+
+  public async cargarPersonajes() {
+    const result = await Storage.get({ key: this.STORAGE_KEY });
+    if (result.value) {
+      this.personajes = JSON.parse(result.value);
+    }
+  }
 
   //constructor() {}
   public obtenerPersonaje() {
     const id = Math.floor(Math.random() * 671) + 1; // Genera un número aleatorio entre 1 y 671
 
-    this.http.get(`https://rickandmortyapi.com/api/character/${id}`).subscribe((data: any) => {this.personajes.push(data);});
+    this.http.get(`https://rickandmortyapi.com/api/character/${id}`).subscribe((data: any) => {
+      this.personajes.push(data);
+      this.guardarPersonajes();
+    });
   }
-
 
   private async mostrarAlerta(header: string, message: string) {
     const alert = await this.alertController.create({
@@ -34,7 +54,7 @@ export class HomePage {
     });
     await alert.present();
   }
-  
+
   public async eliminarPersonaje(personaje: any) {
     const alert = await this.alertController.create({
       header: 'Confirmación',
@@ -53,27 +73,26 @@ export class HomePage {
             const index = this.personajes.indexOf(personaje);
             if (index !== -1) {
               this.personajes.splice(index, 1);
+              this.guardarPersonajes()
             }
-  
+
             // Mostrar una alerta de eliminación exitosa
             this.mostrarAlerta('Eliminado', 'El personaje ha sido eliminado correctamente.');
           }
         }
       ]
     });
-  
+
     await alert.present();
   }
-  
 
-  public setOpen(set:boolean, personaje:any)
-  {
+
+  public setOpen(set: boolean, personaje: any) {
     this.isModalOpen = set;
     this.personaje = personaje
   }
 
-  public setClose(set:boolean, personaje:any)
-  {
+  public setClose(set: boolean, personaje: any) {
     this.isModalOpen = set;
     this.personaje = null;
   }
